63a9d79b875e4c265e597f25386cfa197fc25939
mariano alvarez <mariano@codingco.io>
2023-10-03 17:22:37 -0300

    pokemon card styles

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -6,20 +6,22 @@ import {
   Pressable,
   TouchableWithoutFeedback,
 } from "react-native";
-import React from "react";
+import { capitalize } from "lodash";
+import getColorByPokemonType from "../utils/getColorByPokemonType";
 
 export default function PokemonCard(props) {
   const { pokemon } = props;
-
+  const pokemonColor = getColorByPokemonType(pokemon.type);
+  const bgStyles = { backgroundColor: pokemonColor, ...styles.bgStyles };
   const goToPokemon = () => {};
 
   return (
     <TouchableWithoutFeedback onPress={goToPokemon}>
       <View style={styles.card}>
         <View style={styles.spacing}>
-          <View style={styles.bgStyles}>
+          <View style={bgStyles}>
             <Text style={styles.num}>#{`${pokemon.order}`.padStart(3, 0)}</Text>
-            <Text style={styles.name}>{pokemon.name}</Text>
+            <Text style={styles.name}>{capitalize(pokemon.name)}</Text>
             <Image source={{ uri: pokemon.image }} style={styles.image} />
           </View>
         </View>
@@ -38,7 +40,9 @@ const styles = StyleSheet.create({
     padding: 5,
   },
   bgStyles: {
-    backgroundColor: "grey",
+    flex: 1,
+    borderRadius: 15,
+    padding: 10,
   },
   num: {
     position: "absolute",
